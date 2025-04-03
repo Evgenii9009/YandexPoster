de7fd3c95158dec2b571b17153f61283a5d2499e
Eugene Fedorov <uptiik02@gmail.com>
2025-04-03 18:38:54 +0300

    Add Event model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from catalog.models import Event
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'longitude', 'latitude')
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 
+class Event(models.Model):
+
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        null=True,
+        blank=True
+    )
+    image = models.ImageField(
+        verbose_name='Фото события',
+        null=True, blank=True
+    )
+    short_description = models.TextField(
+        verbose_name='Краткое описание',
+        blank=True,
+    )
+    long_description = models.TextField(
+        verbose_name='Длинное описание',
+        blank=True,
+    )
+    longitude = models.FloatField(
+        verbose_name='долгота',
+        null=True
+    )
+    latitude = models.FloatField(
+        verbose_name='широта',
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Событие'
+        verbose_name_plural = 'События'
+
+    def __str__(self):
+        return self.title
