6cd3ad4ef90b2ba7cac3dfd79726f9c06b1d38e0
Eugene Fedorov <uptiik02@gmail.com>
2025-04-04 11:21:33 +0300

    Add Inline to Event model

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
 from catalog.models import Event, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ('title', 'longitude', 'latitude', 'id')
-
+    inlines = [
+        ImageInline,
+    ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('event_name', 'number')
+
+
