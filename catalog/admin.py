bcde6a5e8a669eab2666b65876e2e2d12fe566c7
Eugene Fedorov <uptiik02@gmail.com>
2025-04-04 22:37:32 +0300

    Add sortage and inlines to admin page

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
+from adminsortable2.admin import SortableAdminBase
+from adminsortable2.admin import SortableStackedInline
 from catalog.models import Event, Image
+from django.utils.html import format_html
 
 
-class ImageInline(admin.TabularInline):
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    fields = ('event_name', 'event_image', 'number')
+    readonly_fields = ['event_image']
+    def event_image(self, obj):
+        image_height=obj.image.height if obj.image.height<=200 else 200
+        image_width = obj.image.width if obj.image.height<200 else obj.image.width*200/obj.image.height
+        return format_html('<img src="{url}" width="{width}" height={height} />',
+            url = obj.image.url,
+            width=image_width,
+            height=image_height
+            )
+
+
+class ImageStackedInline(SortableStackedInline):
     model = Image
 
 
 @admin.register(Event)
-class EventAdmin(admin.ModelAdmin):
+class EventAdmin(SortableAdminBase, admin.ModelAdmin):
     list_display = ('title', 'longitude', 'latitude', 'id')
-    inlines = [
-        ImageInline,
-    ]
-
-@admin.register(Image)
-class ImageAdmin(admin.ModelAdmin):
-    list_display = ('event_name', 'number')
+    inlines = [ImageStackedInline]
 
 
--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.conf import settings
+
 
 class Event(models.Model):
 
@@ -11,7 +11,6 @@ class Event(models.Model):
     )
     place_id = models.CharField(max_length=100, 
                                 null=True)
-    detailsUrl = models.CharField(max_length=200, null=True)
     short_description = models.TextField(
         verbose_name='Краткое описание',
         blank=True,
@@ -61,3 +60,4 @@ class Image(models.Model):
     class Meta:
         verbose_name = 'Фото события'
         verbose_name_plural = 'Фото событий'
+        ordering = ('number',)
