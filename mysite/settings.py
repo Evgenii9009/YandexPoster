1e653fefac61c5b059d7a2bb3294adbb179bdf1a
Eugene Fedorov <uptiik02@gmail.com>
2025-04-07 20:24:16 +0300

    Add default settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,9 +9,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
 
 INSTALLED_APPS = [
