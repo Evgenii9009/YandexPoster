25ba205a71c3cb36b65f7cbd7f9287c2671a8cc1
Eugene Fedorov <uptiik02@gmail.com>
2025-04-25 10:48:53 +0300

    Update load_place

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -22,28 +22,30 @@ class Command(BaseCommand):
         place = response.json()
         try:
             event, _ = Event.objects.get_or_create(title=place["title"],
-                                                   short_description=place["description_short"],
-                                                   long_description=place["description_long"],
-                                                   latitude=place["coordinates"]["lat"],
-                                                   longitude=place["coordinates"]["lng"])
+                                                   defaults = {
+                                                       "long_description": place["description_long"],
+                                                       "short_description": place["description_short"],
+                                                       "longitude": place["coordinates"]["lng"],
+                                                       "latitude": place["coordinates"]["lat"],
+                                                   })
+            for image_number, image_url in enumerate(place["imgs"]):
+                try:
+                    response = requests.get(image_url)
+                    response.raise_for_status()
+                    name = image_url.split("/")[-1]
+                    image = ContentFile(response.content, name)
+                    try:
+                        new_image, _ = Image.objects.get_or_create(image=image,
+                                                                   number=image_number,
+                                                                   event_name=event)
+                        new_image.image.save(name,
+                                             image,
+                                             save=True)
+                    except MultipleObjectsReturned:
+                        print(f'Изображение {image_number} уже есть в базе данных!')
+                except ConnectionError:
+                    pass
+                except requests.exceptions.HTTPError:
+                    pass
         except MultipleObjectsReturned:
             print('Событие уже есть в базе данных!')
-        for image_number, image_url in enumerate(place["imgs"]):
-            try:
-                response = requests.get(image_url)
-                response.raise_for_status()
-                name = image_url.split("/")[-1]
-                image = ContentFile(response.content)
-                try:
-                    new_image, _ = Image.objects.get_or_create(image,
-                                                               number=image_number,
-                                                               event_name=event)
-                    new_image.image.save(name,
-                                         image,
-                                         save=True)
-                except MultipleObjectsReturned:
-                    print(f'Изображение {image_number} уже есть в базе данных!')
-            except ConnectionError:
-                pass
-            except requests.exceptions.HTTPError:
-                pass
